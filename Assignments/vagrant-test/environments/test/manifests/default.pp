237bf5da722dc089b57092c9e68a460ad08b61f5
Rashid Darwish <rashid.darwish@volvocars.com>
2021-09-22 22:55:51 +0200

    Execute puppet

---
+++ Assignments/vagrant-test/environments/test/manifests/default.pp
@@ -0,0 +1,21 @@
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update",
+
+  }
+
+  exec { "apt-get-curl":
+    command => "/usr/bin/apt-get install -y curl"
+  }
+
+  exec { "curl-node":
+    command => "/usr/bin/curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -"
+  }
+
+  exec { "apt-get-node":
+    command => "/usr/bin/apt-get install -y nodejs"
+  }
+
+  Exec["apt-update"] -> Package <| |>
+  Exec["apt-get-curl"] -> Package <| |>
+  Exec["curl-node"] -> Package <| |>
+  Exec["apt-get-node"] -> Package <| |>
