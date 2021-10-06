8f92d14b10c69cc709e8e4b2ba6014b3690ad78a
Rashid Darwish <rashid.darwish@volvocars.com>
2021-10-06 23:32:43 +0200

    Add puppet pre stage

--- Assignments/basic-multi-machine-setup/environments/test/manifests/default.pp
+++ Assignments/basic-multi-machine-setup/environments/test/manifests/default.pp
@@ -1,8 +1,28 @@
-Exec { path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/' }
-
-Exec { "apt-get update":
-  command => "apt-get update";
+# create a new run stage to ensure certain modules are included first
+stage { 'pre':
+  before => Stage['main']
 }
+
+# add the baseconfig module to the new 'pre' run stage
+class { 'baseconfig':
+  stage => 'pre'
+}
+
+# set defaults for file ownership/permissions
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+}
+
+# all boxes get the base config
+include baseconfig
+
+# Exec { path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/' }
+#
+# Exec { "apt-get update":
+#   command => "apt-get update";
+# }
 
 node 'appserver' {
   include nodejs
